ad4268c956508729db0ef4613bb581c2005cdbfb
Eneko Díaz Romero <eneko.diaz.romero@gmail.com>
2021-07-23 11:50:25 +0200

    Add up and down arrow handling
    When pressing up and down in the terminal it fetches the previous commands from the history

--- src/app/components/terminal/terminal.component.ts
+++ src/app/components/terminal/terminal.component.ts
@@ -1,5 +1,6 @@
 import { ViewChild, Component, AfterViewInit, ViewEncapsulation } from '@angular/core';
 import { NgTerminal } from 'ng-terminal';
+import { CommandService } from 'src/app/services/command.service';
 
 @Component({
   selector: 'app-terminal',
@@ -12,10 +13,14 @@ export class TerminalComponent implements AfterViewInit {
   @ViewChild("terminal", { static: true }) terminal!: NgTerminal
   private typingIndicator: string
   private command: string
+  private history: string[]
+  private historyIndex: number
 
-  constructor() { 
+  constructor(private commandService: CommandService) { 
     this.typingIndicator = "$ "
     this.command = ""
+    this.history = []
+    this.historyIndex = 0
   }
 
   ngAfterViewInit() {
@@ -29,15 +34,26 @@ export class TerminalComponent implements AfterViewInit {
       switch (keyboardEvent.key) {
         case "Enter": this.enterKeyTapped(); break
         case "Backspace": this.backspaceKeyTapped(); break
+        case "ArrowUp": this.arrowTapped("up"); break
+        case "ArrowDown": this.arrowTapped("down"); break
+        case "ArrowLeft": this.arrowTapped("left"); break
+        case "ArrowRight": this.arrowTapped("right"); break
         default: this.keyTapped(keyboardEvent.key); break
       }
     })
   }
 
   private enterKeyTapped() {
-    this.handleCommand()
-    this.command = ""
-    this.terminal.write('\r\n' + this.typingIndicator)
+    try {
+      this.history.push(this.command)
+      this.handleCommand()
+      this.command = ""
+    } catch (error) {
+      this.terminal.write("\r\x1B[1;31m" + error + "\x1B[0m")
+    } finally {
+      this.terminal.write('\r\n' + this.typingIndicator)
+      this.historyIndex = 0
+    }
   }
 
   private backspaceKeyTapped() {
@@ -49,6 +65,35 @@ export class TerminalComponent implements AfterViewInit {
     }
   }
 
+  private arrowTapped(arrow: string) {
+    switch (arrow) {
+      case "up":
+        const previousCommand = this.getPreviousCommand()
+        if (previousCommand) {
+          this.command = previousCommand
+          this.terminal.write('\x1b[2K\r')   
+          this.terminal.write('\r' + this.typingIndicator + previousCommand)
+        }
+        break
+      case "down":
+        const nextCommand = this.getNextCommand()
+        if (nextCommand) {
+          this.command = nextCommand
+          this.terminal.write('\x1b[2K\r')
+          this.terminal.write('\r' + this.typingIndicator + nextCommand)
+        } else {
+          this.command = ""
+          this.terminal.write('\x1b[2K\r')
+          this.terminal.write('\r' + this.typingIndicator)
+        }
+        break
+      case "left":
+        break
+      case "right":
+        break
+    }
+  }
+
   private keyTapped(key: string) {
     const cursorXPosition = this.terminal.underlying.buffer.active.cursorX - this.typingIndicator.length
     this.command = this.command.slice(0, cursorXPosition) + key + this.command.slice(cursorXPosition)
@@ -56,14 +101,48 @@ export class TerminalComponent implements AfterViewInit {
   }
 
   private handleCommand() {
-    console.log("Command entered:")
-    console.log(this.command)
+    const command = this.command
+    if (command.length > 0) {
+      switch (command) {
+        case "clear": 
+          this.terminal.underlying.reset()
+          break
+        case "eneko":
+          const result = this.commandService.execute(command)
+          this.terminal.write('\r\n')
+          this.terminal.write(result)
+          break
+      }
+    }
+  }
+
+  private getPreviousCommand(): string | null {
+    let command = null
+    if (this.historyIndex < this.history.length) {
+      this.historyIndex += 1
+      const lastIndex = this.history.length
+      const index = lastIndex - this.historyIndex
+      command = this.history[index]
+    }
+    return command
+  }
+
+  private getNextCommand(): string | null {
+    let command = null
+    if (this.historyIndex >= 0) {
+      this.historyIndex -= 1
+      const lastIndex = this.history.length - 1
+      const index = lastIndex - this.historyIndex
+      command = this.history[index]
+    }
+    return command
   }
 
   private printWelcomeMessage() {
     this.terminal.write("\nHey there! Welcome to eneko-cli web version.")
-    this.terminal.write("\r\nUse this terminal to get all the information about me, from my age to even a full curriculum.")
+    this.terminal.write("\r\nUse this terminal to get all the information about me, starting from my age to even downloading a full curriculum.")
     this.terminal.write("\r\n\nGet started by tapping 'eneko help' to get a list of available commands")
+    this.terminal.write("\r\n\n\x1B[1;32mNote\x1B[0m: you can clear the console by typing 'clear'")
     this.terminal.write('\r\n\n' + this.typingIndicator)
   }
 
