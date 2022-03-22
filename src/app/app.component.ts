0952617944b66613e3671b2d84b50604bf4cb577
Eneko Diaz Romero <eneko.diaz.romero@gmail.com>
2022-03-22 01:44:43 +0100

    Delete unused content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Eneko Díaz Romero - Apple developer';
-}
+export class AppComponent {}
--- src/app/components/warning-alert/warning-alert.component.ts
+++ src/app/components/warning-alert/warning-alert.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-warning-alert',
   templateUrl: './warning-alert.component.html',
   styleUrls: ['./warning-alert.component.css']
 })
-export class WarningAlertComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class WarningAlertComponent {}
