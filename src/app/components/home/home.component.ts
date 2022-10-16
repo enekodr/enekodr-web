357ff28e19946473d1221faaa1c08e8ccd303e63
Eneko Díaz Romero <eneko.diaz.romero@gmail.com>
2022-10-16 10:00:12 +0200

    chore: update packages (#5)
    * chore: update packages
    
    * chore: fix lint and ci
    
    * chore: update node version in ci workflow
    
    * chore: add headless chrome to karma configuration

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,19 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   public android: boolean
 
   constructor() { 
     this.android = /Android/.test(navigator.userAgent)
   }
-
-  ngOnInit(): void {
-  }
-
 }
